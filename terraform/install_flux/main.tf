9ab8c5895757a7d928c6c0064d5a52f3909d1506
Louis Labeyrie <labeyrielouis@gmail.com>
2024-04-03 18:15:31 +0200

    Update to kubernetes

--- terraform/install_flux/main.tf
+++ terraform/install_flux/main.tf
@@ -1,6 +1,17 @@
-module "flux" {
-  source  = "./module"
-  github_repository = var.github_repository
-  github_org = var.github_org
-  github_token = var.github_token
+resource "tls_private_key" "flux" {
+  algorithm   = "ECDSA"
+  ecdsa_curve = "P256"
 }
+resource "github_repository_deploy_key" "this" {
+  title      = "Flux"
+  repository = var.github_repository
+  key        = tls_private_key.flux.public_key_openssh
+  read_only  = "false"
+}
+
+resource "flux_bootstrap_git" "this" {
+  depends_on = [github_repository_deploy_key.this]
+  path       = "clusters/flux-e2e"
+}
+
+// terraform apply -var "github_org=<username or org>" -var "github_token=<token>" -var "github_repository=fleet-infra"
