9b9cedb7c49720376f64a1dadfd1e39e4691002b
Louis Labeyrie <labeyrielouis@gmail.com>
2024-04-03 18:15:31 +0200

    Update to kubernetes

--- terraform/install_flux/versions.tf
+++ terraform/install_flux/versions.tf
@@ -2,9 +2,42 @@ terraform {
   required_version = ">=1.1.5"
 
   required_providers {
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">=2.9.0"
+    flux = {
+      source = "fluxcd/flux"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.27.0"
+    }
+    github = {
+      source  = "integrations/github"
+      version = ">=5.18.0"
     }
   }
 }
+
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+}
+
+
+provider "github" {
+  owner = var.github_org
+  token = var.github_token
+}
+
+# provider "flux" {
+#   kubernetes = {
+#     host                   = kubernetes.cluster.server
+#     client_certificate     = kubernetes.cluster.client_certificate
+#     client_key             = kubernetes.cluster.client_key
+#     cluster_ca_certificate = kubernetes.cluster.cluster_ca_certificate
+#   }
+#   git = {
+#     url = "ssh://git@github.com/${var.github_org}/${var.github_repository}.git"
+#     ssh = {
+#       username    = "git"
+#       private_key = tls_private_key.flux.private_key_pem
+#     }
+#   }
+# }
