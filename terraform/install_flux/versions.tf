79c73c4b04e3c540d1d08454e4a77356ed5fd0ab
Louis Labeyrie <labeyrielouis@gmail.com>
2024-04-03 18:35:07 +0200

    Update versions.tf to use local kube.conf file for Flux provider

--- terraform/install_flux/versions.tf
+++ terraform/install_flux/versions.tf
@@ -5,19 +5,11 @@ terraform {
     flux = {
       source = "fluxcd/flux"
     }
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-      version = "2.27.0"
-    }
     github = {
       source  = "integrations/github"
       version = ">=5.18.0"
     }
   }
-}
-
-provider "kubernetes" {
-  config_path    = "~/.kube/config"
 }
 
 
@@ -26,18 +18,15 @@ provider "github" {
   token = var.github_token
 }
 
-# provider "flux" {
-#   kubernetes = {
-#     host                   = kubernetes.cluster.server
-#     client_certificate     = kubernetes.cluster.client_certificate
-#     client_key             = kubernetes.cluster.client_key
-#     cluster_ca_certificate = kubernetes.cluster.cluster_ca_certificate
-#   }
-#   git = {
-#     url = "ssh://git@github.com/${var.github_org}/${var.github_repository}.git"
-#     ssh = {
-#       username    = "git"
-#       private_key = tls_private_key.flux.private_key_pem
-#     }
-#   }
-# }
+provider "flux" {
+  kubernetes = {
+    config_path = "./kube.conf"
+  }
+  git = {
+    url = "ssh://git@github.com/${var.github_org}/${var.github_repository}.git"
+    ssh = {
+      username    = "git"
+      private_key = tls_private_key.flux.private_key_pem
+    }
+  }
+}
