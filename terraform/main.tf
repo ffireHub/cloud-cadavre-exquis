cb7a3ca65b56df18c954928113827fe2120262f1
Louis Labeyrie <labeyrielouis@gmail.com>
2024-04-03 20:16:32 +0200

    Add instance variable to Install_instance_and_kube module

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,6 @@
 module "Install_instance_and_kube" {
   source = "./setup_kube"
+  instance = var.instance
   OS_APPLICATION_KEY = var.OS_APPLICATION_KEY
   OS_APPLICATION_SECRET = var.OS_APPLICATION_SECRET
   OS_CONSUMER_KEY = var.OS_CONSUMER_KEY
