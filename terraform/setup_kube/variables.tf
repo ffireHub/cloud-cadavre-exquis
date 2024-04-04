430f97cd085bbebce4f1c6cb0c20bccd21074895
mathias <mathiasdurat@gmail.com>
2024-04-04 17:13:19 +0200

    changing version to non snapshot

--- terraform/setup_kube/variables.tf
+++ terraform/setup_kube/variables.tf
@@ -13,5 +13,5 @@ variable "OS_CONSUMER_KEY" {type = string}
 variable "ssh_public_keys" {
   description = "List of SSH public keys"
   type        = list(string)
-  default     = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKLQ1UxlE179UUnjsjUKbboy47frMa9cDRue16f+71UX pauline.contat01@etu.umontpellier.fr"]
+  default     = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHzod2RRe8nJQa0jIo863fd30B/7nicdZXwW3jbMD3FP mathiasdurat@gmail.com"]
 }
