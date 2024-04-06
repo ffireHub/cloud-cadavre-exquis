669782d4db3de2722e0481017b706b9ebb568019
Louis Labeyrie <labeyrielouis@gmail.com>
2024-04-07 01:08:00 +0200

    Update ssh_public_keys default value

--- terraform/setup_kube/variables.tf
+++ terraform/setup_kube/variables.tf
@@ -13,5 +13,5 @@ variable "OS_CONSUMER_KEY" {type = string}
 variable "ssh_public_keys" {
   description = "List of SSH public keys"
   type        = list(string)
-  default     = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHzod2RRe8nJQa0jIo863fd30B/7nicdZXwW3jbMD3FP mathiasdurat@gmail.com"]
+  default     = [""]
 }
